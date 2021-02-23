3aa6f30dae2fce0dcc27766b8064783b3f09d001
Sanyi <hunited@gmail.com>
2021-02-23 09:33:49 +0100

    Employees adattábla szépítgetés

--- src/main/resources/db/migration/V1__employees.sql
+++ src/main/resources/db/migration/V1__employees.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `employees`(`id` BIGINT auto_increment, `emp_name` VARCHAR(255), CONSTRAINT primary key(id));
+CREATE TABLE `employees`(`id` BIGINT AUTO_INCREMENT, `emp_name` VARCHAR(255), CONSTRAINT PRIMARY KEY(`id`));
 INSERT INTO `employees` (`emp_name`) VALUES
 	('John Doe'),
 	('Jane Doe'),
